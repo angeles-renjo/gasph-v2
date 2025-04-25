39e9fbcd4e8082d422d3b7f365eea63a55957d85
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2025-04-25 23:48:03 +0800

    Revert "workaround: for ios lag"
    This reverts commit 9f65f5aee314a7c87358c6bf6081ddeed5bf905b.

--- hooks/useGoogleMapIosPerfFix.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Placeholder hook for non-iOS platforms.
- * The actual performance fix logic is in useGoogleMapIosPerfFix.ios.ts
- * @see https://github.com/react-native-maps/react-native-maps/issues/4937
- */
-export const useGoogleMapIosPerfFix = () => {
-  // No-op on non-iOS platforms
-};
